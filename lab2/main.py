17914bb46b6df932d73344a5566267565f4da01e
Kirilenko Uliana <125649315+Uliana0202@users.noreply.github.com>
2024-10-28 05:13:28 +0300

    Проверка на скорую достижимость эквивалентности теперь связана с числом ветвлений.
    Число классов эквивалентности - число ветвлений. Я предполагаю, что если у меня число классов эквивалентности впятеро меньше, чем число ветвлений, то до эквивалентности еще далеко (+ условия, что если у меня только "" суффикс, то дополнительное расширение не очень информативно, к тому же дополнительное расширение очень быстро увеличивает расширенную часть, так что его лучше делать не больше 3х).
    
    Для улучшения оптимизации можно менять 2 параметра:
    1. "впятеро"
    2. "не больше 3х"
    Но тут без МАТа сложно подобрать.

--- lab2/main.py
+++ lab2/main.py
@@ -21,65 +21,73 @@ def print_table(table,start_extension):
         print()
     print()
 
-def generate_strings(len, current_string):
-    if len == 0:
-        return [current_string]
-
-    strings = []
-    for c in ['L', 'R']:
-        strings.extend(generate_strings(len - 1, current_string + c))
-
-    return strings
-def make_extension(table, pref, n):
-    for i in range(n):
-        strings = generate_strings(i+1, "")
-        for s in strings:
-            table.append(["0" for _ in range(len(table[0]))])
-            table[-1][0] = pref + s
-
-
-def solve_incompleteness(table, pos_unchecked, start_extension, coef_extension):
+def make_extension(table, pref):
+    strings = ["L", "R"]
+    for s in strings:
+        table.append(["0" for _ in range(len(table[0]))])
+        table[-1][0] = pref + s
+    full_rows(len(table) - 2, table)
+def does_not_meet(table, finish, row):
+    for i in range(1, finish):
+        if table[i][1:] == row:
+            return False
+    return True
+def solve_incompleteness(table, pos_unchecked, start_extension):
     start_extension_0 = start_extension
     while True:
         for i in range(pos_unchecked, len(table)):
-            do_not_meet = True
-            j = 1
-            while j < start_extension and do_not_meet:
-                if table[i][1:] == table[j][1:]:
-                    do_not_meet = False
-                j += 1
-
-            if do_not_meet:
+            if does_not_meet(table, start_extension, table[i][1:]):
                 table[i], table[start_extension] = table[start_extension], table[i]
                 start_extension += 1
 
         if start_extension == start_extension_0:
             return True, start_extension
 
-        len_0 = len(table)
         for i in range(start_extension - start_extension_0):
-            make_extension(table, table[start_extension_0 + i][0], coef_extension)
+            make_extension(table, table[start_extension_0 + i][0])
 
-        full_rows(len_0, table)
-
-        flag, start_extension = solve_incompleteness(table, len(table) - 2 * (start_extension - start_extension_0), start_extension, coef_extension)
+        flag, start_extension = solve_incompleteness(table, len(table) - 2 * (start_extension - start_extension_0), start_extension)
         if flag:
             return True, start_extension
-
-
 def main():
+    num_of_vertices = 16  ################### Read from a file
     table = [
         ["", ""],
-        ["", "0"]
+        ["", "0"],
+        ["L", "0"],
+        ["R", "0"]
     ]
-    coef_extension = 1
-    start_extension = 2
-    make_extension(table, table[0][1], coef_extension)
     full_rows(1, table)
+    start_extension = 2
+    print_table(table, start_extension)
+
+    requirement_for_extra = 5*(start_extension - 1) < num_of_vertices
+    start_extra = start_extension
+    len_extra = len(table[-1][0])
 
     while True:
-        _, start_extension = solve_incompleteness(table, start_extension, start_extension, coef_extension)
+        _, start_extension = solve_incompleteness(table, start_extension, start_extension)
+        print_table(table, start_extension)
+
+        # SxSigma быстро возрастает, поэтому ставлю предел в 2 дополительных расширения
+        counter = 0
+        while counter < 2 and requirement_for_extra and len(table[0]) > 2:
+            len_0 = len(table)
+            extended = 0
+            for j in range(start_extra, len_0):
+                if len(table[j][0]) >= len_extra:
+                    make_extension(table, table[j][0])
+                    extended += 1
+            counter += 1
+            start_extra = len(table) - 2*extended
+            start_extension_0 = start_extension
+            _, start_extension = solve_incompleteness(table, start_extension, start_extension)
+            len_extra += 1
+
+        start_extra = len(table) # Дальше дополнительно расширять мне надо только те части таблицы, которые получились из нового класса и дописанных L, R
+        len_extra = len(table[-1][0])
         guessed, counterexample = is_equivalent(table, start_extension)
+
 
         if guessed:
             break
@@ -88,11 +96,6 @@ def main():
             table[0].append(counterexample[:i])
             for j in range(1, len(table)):
                 table[j].append(is_member(table[j][0] + counterexample[:i]))
-                
-        if len(counterexample) > 2*len(table[-1][0]):
-            coef_extension += 1
-        else:
-            coef_extension = 1
 
     print_table(table, start_extension)
 
